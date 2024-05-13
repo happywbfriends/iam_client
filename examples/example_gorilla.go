a9516900e85657fe9e28b73e90a8311dd4d406c0
Mike Wozowoy <vozovoy.mihail@wb.ru>
2024-05-13 11:30:09 +0300

    Добавлены примеры, описания методов и т.п.

--- examples/example_gorilla.go
+++ examples/example_gorilla.go
@@ -11,7 +11,8 @@ func gorillaMuxRouter(r *mux.Router) {
 	// Ручки роутера
 	r.HandleFunc("/api/v1/admin/actionLog", someHandler).Methods("GET")
 	r.HandleFunc("/api/v1/admin/auth-services", someHandler).Methods("GET")
-	r.HandleFunc("/api/v1/admin/categories/{id}", someHandler).Methods("POST")
+	r.HandleFunc("/api/v1/admin/category/{id}", someHandler).Methods("GET")
+	r.HandleFunc("/api/v1/admin/category/{id}", someHandler).Methods("POST")
 
 	// Клиент IAM
 	// serviceId - id нашего сервиса
@@ -20,19 +21,42 @@ func gorillaMuxRouter(r *mux.Router) {
 	logger := IamLogger{logger: zerolog.Logger{}}
 	iamClient := iam_client.New(serviceId, cfg, logger)
 
-	// Создаем матрицу прав. Подробности см. в описании функций iam_client.NewPermissionsChecker() и
+	// Матрица прав доступа. Подробности см. в описании функций iam_client.NewPermissionsChecker() и
 	// PermissionChecker.AuthMiddlewareHandler().
+	// Алгоритм:
+	// * с правом доступа "admin:*" пускаем ко всем ручкам
+	// * с правом доступа "view:*" пускаем ко всем GET-ручкам
+	// * для всех остальных прав применяется матрица доступа
 	permissionsMatrix := make(map[string][]string)
-	permissionsMatrix["GET/api/v1/admin/actionLog"] = []string{"view:log", "admin"}
+
+	// Разрешен доступ с правами:
+	// "admin:*", "view:*", "view:log", "admin:log"
+	permissionsMatrix["GET/api/v1/admin/actionLog"] = []string{"view:log", "admin:log"}
+
+	// Если у разрешенного права опущен скоуп, пускаем с любым скоупом, т.е. в данном случае
+	// подойдет любой вариант из: "admin:*", "admin:view" и т.п.
+	// Т.о. разрешен доступ с правами:
+	// "admin:*", "view:*", "admin:scope1", "admin:scope2", ...
 	permissionsMatrix["GET/api/v1/admin/auth-services"] = []string{"admin"}
-	permissionsMatrix["POST/api/v1/admin/categories/{id}"] = []string{"admin"}
+
+	// Разрешен доступ с правами:
+	// "admin:*", "view:*", "view:log"
+	permissionsMatrix["GET/api/v1/admin/category/{id}"] = []string{"view:log"}
+
+	// POST-ручка, своего правила нет, так что в данном случае будет разрешен доступ только с правом "admin:*".
+	// Если строку раскомментировать, будет разрешен доступ с правами:
+	// "admin:*", "edit:category"
+	// permissionsMatrix["POST/api/v1/admin/category/{id}"] = []string{"edit:category"}
+
+	// Проверяльщик прав доступа
 	iamPermissionsChecker := iam_client.NewPermissionsChecker(permissionsMatrix, logger)
-	// Без вызова этой функции не будут работать пути роутера с макросами
+	// Вызов WithGorillaMuxRouter нужен, чтобы заработали пути роутера с макросами
 	iamPermissionsChecker.WithGorillaMuxRouter(r)
 
 	r.Use(
 		// Аутентификация пользователя (по ключу ИЛИ по кукам)
 		iamClient.AuthMiddlewareHandler,
+
 		// AuthMiddlewareHandler должен использоваться после аутентификации в IAM-клиенте.
 		// С правом доступа "admin:*" пускает ко всем ручкам.
 		// С правом доступа "view:*" пускает ко всем GET-ручкам.
